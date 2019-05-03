1b118f3703ae6174f4dcba53108bd1319505bf10
Vivien <vdebona@umanit.fr>
2019-05-03 12:28:35 +0200

    Add a local user "umanit"
    - The UID will be 9001, except if the var env LOCAL_USER_ID is passed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,8 @@ if [ $? -ne 0 ]; then
   echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
 fi
 
+# add local user "umanit"
+[[ -z "${LOCAL_USER_ID}" ]] && USER_ID=9001 || USER_ID="${LOCAL_USER_ID}"
+adduser -s /bin/bash -u $USER_ID -D -h /home/umanit umanit
+
 exec "$@"
